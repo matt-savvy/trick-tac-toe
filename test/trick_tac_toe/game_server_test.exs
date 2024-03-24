b72c1a329c7ae8606e2abcf425f29bfd23a4e717
Matt Savoia <matt@1-800-rad-dude.com>
2024-03-24 13:17:33 -0400

    Refactor GameServerTest, prefer integration test

--- test/trick_tac_toe/game_server_test.exs
+++ test/trick_tac_toe/game_server_test.exs
@@ -3,23 +3,10 @@ defmodule TrickTacToe.GameServerTest do
 
   alias TrickTacToe.{Game, GameServer}
 
-  describe "start_link/1" do
-    test "starts with new game" do
-      new_game = Game.new()
+  test "integration test" do
+    new_game = Game.new()
 
-      {:ok, pid} = GameServer.start_link(nil)
-
-      assert new_game == :sys.get_state(pid)
-    end
-  end
-
-  describe "get_state/1" do
-    test "returns state" do
-      new_game = Game.new()
-
-      pid = start_supervised!(GameServer)
-
-      assert new_game == GameServer.get_state(pid)
-    end
+    assert {:ok, pid} = GameServer.start_link(nil)
+    assert new_game == GameServer.get_state(pid)
   end
 end
