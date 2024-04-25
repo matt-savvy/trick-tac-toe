3f8f1e9399e8f987bb0ec27751ad21f571febd96
Matt Savoia <matt@1-800-rad-dude.com>
2024-04-25 19:07:26 -0400

    Update GameSupervisor, get id using :global lock

--- lib/trick_tac_toe/game_supervisor.ex
+++ lib/trick_tac_toe/game_supervisor.ex
@@ -8,16 +8,28 @@ defmodule TrickTacToe.GameSupervisor do
   end
 
   def new_game do
-    game_id = next_id()
+    :global.trans(
+      {:new_game, self()},
+      fn ->
+        game_id = next_id()
 
-    with {:ok, pid} <- DynamicSupervisor.start_child(__MODULE__, {GameServer, game_id}) do
-      game = GameServer.get_state(pid)
-      {:ok, game, game_id}
-    end
+        with {:ok, pid} <- DynamicSupervisor.start_child(__MODULE__, {GameServer, game_id}) do
+          game = GameServer.get_state(pid)
+          {:ok, game, game_id}
+        end
+      end
+    )
   end
 
   defp next_id do
-    System.unique_integer([:monotonic, :positive])
+    do_next_id(0)
+  end
+
+  defp do_next_id(id) do
+    case GameServer.get_state(id) do
+      {:error, :not_found} -> id
+      {:ok, _state} -> do_next_id(id + 1)
+    end
   end
 
   def list_games do
